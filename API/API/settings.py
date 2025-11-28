cc8252c45af9d9d4a8e3f1838cc74d4f16c55837
rellisreal <gendarkdragon@gmail.com>
2025-11-28 20:32:13 +0800

    Minor changes, playing around with pre-commit

--- API/API/settings.py
+++ API/API/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
-from dotenv import load_dotenv
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +29,7 @@ SECRET_KEY = os.getenv("DEV_SECRETKEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = ["127.0.0.1", "localhost"]
 
 
 # Application definition
